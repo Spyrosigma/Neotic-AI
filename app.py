5d16b81899d0277dda728cddf641ba20c7154a5d
Satyam Namdev <namdev2003satyam@gmail.com>
2024-07-13 12:35:24 +0530

    Deployment stuff

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
